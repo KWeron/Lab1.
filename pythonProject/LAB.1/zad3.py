1251888e3d2748c7d67167151f8d0ae11b12df40
KWeron <w70820@student.wsiz.edu.pl>
2023-11-09 09:08:01 +0100

    Dodano rozwiązanie zadanie 3.

--- pythonProject/LAB.1/zad3.py
+++ pythonProject/LAB.1/zad3.py
@@ -3,7 +3,6 @@ a=int(input("podaj długość boku a prostokąta: "))
 b=int(input("podaj długość boku b prostokąta: "))
 # p - pole prostokąta
 # o - obwód prostokąta
-
 p=a*b
 o=2*a+2*b
 print(f"Pole prostokąta wynosi {int(p)}, natomimast obwód wynosic {int(o)}")
