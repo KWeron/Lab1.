c0d2d2a8bdc7bf5518df110ece9a2d752b4a1b54
KWeron <w70820@student.wsiz.edu.pl>
2023-11-13 02:41:28 +0100

    Poprawione zadania 6 (pętla while).

--- pythonProject2/zad.pętla.6.py
+++ pythonProject2/zad.pętla.6.py
@@ -9,12 +9,15 @@ s=0
 
 while x <= n:
     o = int(input(f"Podaj liczbę punktów studenta {x}: "))
-    if 0<=o<=100:
-        s += o
-        x += 1
+    if 0>o:
+        print("Nieprawidłowo podana wartość. Nie może być wartością ujemną.")
+        continue
+    elif o>100:
+        print("Nieprawidłowo podana wartość. Nie może być więcej niż 100.")
         continue
     else:
-        break
+        s += o
+        x += 1
 
 ś=s/n
 print("Średnia liczba punktów w grupie wynosi: ",ś)
