b09f688deccb735ccefc63743124449c48fba248
KWeron <w70820@student.wsiz.edu.pl>
2023-12-23 16:04:13 +0100

    Dodanie zadania 4.

---
+++ pythonProject1/Zad.4.py
@@ -0,0 +1,29 @@
+#a
+#imie = str(input("Podaj swoję imię: "))
+#print("Witaj",imie)
+
+#b
+#wiek = int(input("Podaj swój wiek: "))
+#print("Twój wiek to",wiek)
+
+#c
+#imie1 = str(input("Podaj imię: "))
+#nazwisko = str(input("Podaj nazwisko: "))
+#inicjaly = imie1[0].upper() + nazwisko[0].upper()
+#print("Twoje inicjały to:", inicjaly)
+
+#d
+tekst = "Wesołych świąt. "
+print(tekst*5)
+
+#e
+tekst1 = "Wesołych świąt "
+tekst2 = "i szczęśliwego Nowego Roku."
+tekst3 = tekst1 + tekst2
+print(tekst1+tekst2+"\n"+tekst3)
+
+#f
+tekst5 = "Zbliża się nowy rok. "
+tekst6 = "Rok 2024 będzie niezapminany."
+tekst7=tekst5[:len(tekst5)//2]+tekst6[len(tekst6)//2:]
+print(tekst5+tekst6+'\n'+tekst7)
