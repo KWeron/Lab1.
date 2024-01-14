dfefc62d444b53b067562140eb2d372cf6178907
KWeron <w70820@student.wsiz.edu.pl>
2024-01-14 21:21:18 +0100

    Dodanie poprawionego rozwiązania zadania dodatkowego numer 1.

--- pythonProject4/Zad.dod.1.py
+++ pythonProject4/Zad.dod.1.py
@@ -1,8 +1,12 @@
-def imie(x):
+def plec(x):
     ostatniaLitera=x[-1]
     if ostatniaLitera=="a":
         return "jest to kobieta"
     else:
         return "jest to mężczyzna"
 x=str(input("Podaj imię: "))
-print(imie(x))
+print(plec(x))
+
+imiona=["Karolina", "Oskar", "Maksymilian", "Oliwia", "Iwona"]
+pary={imie: plec(imie) for imie in imiona}
+print(pary)
