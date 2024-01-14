aa04e4820fc19ddbea39fdae9291be14d1d5b2d3
KWeron <w70820@student.wsiz.edu.pl>
2024-01-14 23:14:23 +0100

    Dodanie rozwiązania zadania numer 3.

---
+++ pythonProject4/Zad.3.py
@@ -0,0 +1,8 @@
+#zad.3
+def fun(imie, wiek):
+    """Funkcja wypisuje na ekranie podane imię i wiek.
+    :param imie: Imię do wypisania.
+    :param wiek: Wiek do wypisania."""
+    print(input("Podaj imię: "))
+    print(input("Podaj wiek: "))
+print(fun.__doc__)
