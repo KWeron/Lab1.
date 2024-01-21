c32b02f6b2c8779443e0f2fa51dddacd5a861949
KWeron <w70820@student.wsiz.edu.pl>
2024-01-21 17:58:10 +0100

    Dodanie rozwiązania zadania numer 2.

---
+++ pythonProject5/Zad.2.py
@@ -0,0 +1,16 @@
+import math
+#1
+pierwiastek=math.sqrt(81)
+print(pierwiastek)
+#2
+potegowanie=math.pow(8, 10)
+print(potegowanie)
+#3
+dodPotegowania=math.sqrt(2)+math.sqrt(3)+math.sqrt(6)
+print(dodPotegowania)
+#4
+minpierwiastek=(-5) ** (1/2)
+print(minpierwiastek)
+#5
+pierwiastek3Stopnia=math.pow(125, 1/3)
+print(pierwiastek3Stopnia)
