3b0cd416094375e4ea981b8be90342fc73183c91
KWeron <w70820@student.wsiz.edu.pl>
2024-01-21 20:16:23 +0100

    Dodanie rozwiązania zadania numer 6.

---
+++ pythonProject5/Zad.6.py
@@ -0,0 +1,8 @@
+import math
+import keyword
+
+print("Funkcje występujące w module math to: ", dir(math))
+
+print("Funkcje występujące w module tuple to: ", dir(tuple))
+
+print("Funkcje występujące w module keyword to: ", dir(keyword))
