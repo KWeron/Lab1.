1f23bd09abea97dfb2dd12e231e96bf18ff0b546
KWeron <w70820@student.wsiz.edu.pl>
2024-01-30 11:27:01 +0100

    Dodanie rozwiązania zadania 4. z Matplotlib.

--- pythonProject6/Zad.4.Matplotlib.py
+++ pythonProject6/Zad.4.Matplotlib.py
@@ -1,8 +1,8 @@
 import matplotlib.pyplot as plt
-import numpy as np
+
 
 ocena = [1, 3, 4, 6, 2, 4, 2, 1, 6, 4, 4, 5, 2, 5]
-plt.hist(ocena, bins=np.arange(1, 7) , edgecolor='black', alpha=0.7)
+plt.hist(ocena, bins=range(1, 7) , edgecolor='black')
 
 plt.title('Rozkład ocen uczniów')
 plt.xlabel('Ocena')
